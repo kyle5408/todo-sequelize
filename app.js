cfb19f72052de1b35ff5b3fbf350b4dd7ed0a603
kyle5408 <kyle5408@yahoo.com.tw>
2021-08-19 11:52:01 +0800

    chore: setup app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const exhbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverrode = require('method-override')
+const bcrypt = require('bcryptjs')
+
+const app = express()
+const PORT = 3000
+
+// 引用套件
+app.engine('hbs', exhbs({ defaultLayot: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverrode('_method'))
+
+// 設定路由&監聽
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(PORT,() => {
+  console.log(`APP is running on http://localhost:${PORT}`)
+})
