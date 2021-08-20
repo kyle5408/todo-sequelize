121a29ec5a33d3a3bd7a7eafdeff5d3191095650
kyle5408 <kyle5408@yahoo.com.tw>
2021-08-20 11:58:02 +0800

    feat: add login function

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const exhbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const methodOverrode = require('method-override')
+const methodOverride = require('method-override')
+const session = require('express-session')
+// 載入設定檔，會用到session要寫在 express-session 以後
+const passport = require('passport')
+const usePassport = require('./config/passport')
 const bcrypt = require('bcryptjs')
 const db = require('./models')
 const Todo = db.Todo
 const User = db.User
+
 
 const app = express()
 const PORT = 3000
@@ -14,7 +19,15 @@ const PORT = 3000
 app.engine('hbs', exhbs({ defaultLayot: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverrode('_method'))
+app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
 
 // 設定路由&監聽
 app.get('/', (req, res) => {
@@ -53,9 +66,11 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.redirect('/')
-})
+// 加入 middleware，驗證 reqest 登入狀態
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
@@ -64,29 +79,29 @@ app.get('/users/register', (req, res) => {
 app.post('/users/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
   User.findOne({ where: { email } })
-  .then(user => {
-    if (user) {
-      console.log('User already exists')
-      return res.render('register', {
-        name,
-        email,
-        password,
-        confirmPassword
-      })
-    }
-    return bcrypt
-    .genSalt(10)
-    .then(salt => { bcrypt.hash(passwprd, salt)})
-    .then(hash => {
-      User.create({
-        name,
-        email,
-        password: hash
-      })
+    .then(user => {
+      if (user) {
+        console.log('User already exists')
+        return res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      }
+      return bcrypt
+        .genSalt(10)
+        .then(salt => { bcrypt.hash(passwprd, salt) })
+        .then(hash => {
+          User.create({
+            name,
+            email,
+            password: hash
+          })
+        })
+        .then(() => { res.render('/') })
+        .cathch(err => console.log(err))
     })
-    .then(()=>{res.render('/')})
-    .cathch(err => console.log(err))
-  })
 })
 
 app.listen(PORT, () => {
---
+++ config/passport.js
@@ -0,0 +1,41 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
+
+module.exports = app => {
+  app.use(passport.initialize())
+  app.use(passport.session())
+  passport.use(new LocalStrategy({ usernameField: 'email'}, (email, password, done) => {
+    User.findOne({ where: { email }})
+    .then(user => {
+      if(!user) {
+        return done(null, false, { message: 'That email is not registered!'})
+      }
+      return bcrypt.compare(password, user.password)
+      .then(isMatch => {
+        if(!isMatch) {
+          return done(null, false, { message: 'Email or Password incorrect.'})
+        }
+        return done(null, user)
+      })
+    })
+    .catch(err => done(err, false))
+  })
+  )
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+  passport.deserializeUser((id, done) => {
+    //findByPk method obtains only a single entry from the table, using the provided primary key.
+    User.findByPk(id)
+    .then(user => {
+      //toJSON() 將資料轉成plain object
+      // console.log(user)
+      user = user.toJSON()
+      // console.log(user)
+      done(null, user)
+    }).catch(err => done(err, null))
+  })
+}
